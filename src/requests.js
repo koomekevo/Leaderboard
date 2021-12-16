c3b8537e91ffe33aaeb74de4c7f97b8c02a9d6ca
koomekevo <koome.kevo@gurucrafts.net>
2021-12-16 17:18:18 +0300

    Fix linter errors

--- src/requests.js
+++ src/requests.js
@@ -1,19 +1,19 @@
 const getScores = async (id) => {
-    const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
-    const response = await fetch(url, { method: 'GET' });
-    const scores = await response.json();
-    return scores;
-  };
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
+  const response = await fetch(url, { method: 'GET' });
+  const scores = await response.json();
+  return scores;
+};
   
-  const postScores = async (id, body) => {
-    const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
-    await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  };
+const postScores = async (id, body) => {
+  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
   
-  export { getScores, postScores };
+export { getScores, postScores };
